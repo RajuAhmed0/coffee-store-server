71b1e0ebd73366417a803f3f1ea47eb5ba8560f9
RajuAhmed0 <itzraju98@gmail.com>
2025-02-27 14:59:41 +0600

    a

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+require('dotenv').config();
 const port = process.env.PORT || 4000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
 
 
-const uri = "mongodb+srv://rajuahmed5:R0a0j7ugta5@cluster0.c9gyi.mongodb.net/?appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.c9gyi.mongodb.net/?appName=Cluster0`;
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -23,6 +25,28 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const coffeeInfo = client.db("coffee_store").collection("coffee");
+
+
+        app.get("/coffee", async (req, res) => {
+            const result = await coffeeInfo.find().toArray()
+            res.send(result)
+        })
+
+
+        app.get("/coffee/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await coffeeInfo.findOne(query)
+            res.send(result)
+        })
+
+
+
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
